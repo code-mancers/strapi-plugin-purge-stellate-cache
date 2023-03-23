4fe9e4e181a43d5a9aabe8af095b67b832233afb
SyedSibtainRazvi <syedsibtain191@gmail.com>
2023-03-23 12:39:43 +0530

    feat: added box and content layout

--- src/plugins/strapi-plugin-stellate/admin/src/pages/HomePage/index.js
+++ src/plugins/strapi-plugin-stellate/admin/src/pages/HomePage/index.js
@@ -1,18 +1,21 @@
-/*
+/**
  *
  * HomePage
  *
  */
-
 import React from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+import { Box } from "@strapi/design-system/Box";
+import { BaseHeaderLayout, ContentLayout } from "@strapi/design-system/Layout";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
+      <BaseHeaderLayout title="Refresh Stellate cache" as="h2" />
+      <Box background="neutral100">
+        <ContentLayout>
+          <h1>Clear Cache</h1>
+        </ContentLayout>
+      </Box>
     </div>
   );
 };
